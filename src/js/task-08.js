444254cfd2cf685588c11dbf261c41d8d90e207c
MateuszKar2 <system505@protonmail.com>
2023-03-07 18:03:07 +0100

    add file

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,29 @@
+//napisać skrypt zarządzający formularzem logowania
+//opracowanie przesłania formularza form.login-form zgodnie ze zdarzeniem submit
+//podczas przesyłania strona nie powinna się restartować
+//nie uzupełnione pole mają zawierać alert przypominający o uzupełnieniu
+//po wysłaniu formularza przez użytkownika zbieram wartość pól jako obiekt
+//gdzie nazwą pola będzie nazwa właściwości
+//wartością pola- wartość właściwości
+//dostęp do elementów formularza za pomocą właściwości elements
+//obiekt umieszczam w wierszu poleceń 
+//należy wyczyścić wartość pól formularza za pomocą reset
 
+
+const formLogin = document.querySelector('.login-form');
+
+formLogin.addEventListener('submit', e => {
+    e.preventDefault();
+    const formData = {};
+    for (const element of formLogin.elements) {
+        if (element.name) {
+            formData[element.name] = element.value;
+        }
+    }
+        if (formData.email && formData.password) {
+            console.log(formData);
+            formLogin.reset();
+        } else {
+            alert('all fields should be completed');
+        }
+});
