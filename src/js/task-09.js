b63238e5f29df725696ccab0791498aa440865ef
MateuszKar2 <system505@protonmail.com>
2023-03-07 18:25:26 +0100

    add file

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+//skrypt który zmienia kolor elementu body
+//poprzez style inline po kliknięciu na button.change-color
+//wprowadza wartość koloru do span.color
+
+const mainColor = document.querySelector("body");
+const clickColor = document.querySelector('.change-color');
+const changeColor = document.querySelector('.color');
+
+
+
+clickColor.addEventListener("click", (e) => {
+  const randomColor = getRandomHexColor();
+  mainColor.style.backgroundColor = randomColor;
+  changeColor.textContent = randomColor;
+});
