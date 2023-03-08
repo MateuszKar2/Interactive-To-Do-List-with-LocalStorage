6b5fb902fe871418cc804c12870e19e7dca9c61f
MateuszKar2 <system505@protonmail.com>
2023-03-08 23:43:22 +0100

    add plik

---
+++ js/task-07.js
@@ -0,0 +1,16 @@
+//napisać skrypt który reaguje na zmianę wartości input#font-size-control"
+//zmienia styl inline span#text
+//aktualizuje właściwość font-size
+//podczas przesuwania paska bedzie się zmieniał rozmiar tekstu
+
+
+const changeValue = document.querySelector('#font-size-control');
+const onText = document.querySelector('#text');
+
+onText.style.fontSize = changeValue.value + 'px';
+
+const textResizing = () => {
+    onText.style.fontSize = `${changeValue.value}px`
+};
+
+changeValue.addEventListener('input', textResizing);
