d8efd5bd18f96f44ccaa3666f264bb30ed043230
MateuszKar2 <system505@protonmail.com>
2023-05-11 12:50:05 +0200

    test task01

--- js/task-01.js
+++ js/task-01.js
@@ -1,28 +1,25 @@
+
+// const categories = document.querySelector('#categories');
+// const items = categories.querySelectorAll('.item');
+
+// console.log(`Number of categories: ${items.length}`);
+
+// items.forEach(item => {
+//   const title = item.querySelector('h2').textContent;
+//   const elements = item.querySelectorAll('li').length;
+
+//   console.log(`Category: ${title}
+//   Elements: ${elements}`);
+// });
+
 //liczę LICZBĘ kategorii <li.item>
 //wprowadzam do wiersza poleceń LICZBĘ kategorii
 //ITERUJĘ za pomocą pętli forEach
 //ZNAJDUJĘ nagłówki i zwracam zawartość tekstową za pomocą ".textContent"
 //ZNAJDUJĘ i liczę liczbę elementów <li> za pomocą ".length"
 //wprowadzam do wiersza poleceń NAGŁÓWEJ i LICZBĘ elementów
-
-
-
-
-
-
-const categories = document.querySelector('#categories');
-const items = categories.querySelectorAll('.item');
-
-console.log(`Number of categories: ${items.length}`);
-
-items.forEach(item => {
-  const title = item.querySelector('h2').textContent;
-  const elements = item.querySelectorAll('li').length;
-
-  console.log(`Category: ${title}
-  Elements: ${elements}`);
-});
-
-
  
+const amount = document.querySelector('#categories');
+const heading = document.querySelectorAll('.item');
 
+console.log(`Number of categories: ${heading.length}`);
