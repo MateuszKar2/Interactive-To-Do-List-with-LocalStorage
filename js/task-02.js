f5d12443623f4d898e8714405c2b3c5162b20783
MateuszKar2 <system505@protonmail.com>
2023-05-12 14:44:27 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -17,20 +17,19 @@ const ingredients = [
 //umieścić wszystkie elementy <li> na liście <ul> podczas jednej operacji
 //dołączam za pomocą metody .append
 
-
-
-const ingredientsList = document.querySelector(`#ingredients`);
+const ingredientsList = document.querySelector('#ingredients');
 
 const items = ingredients.map(ingredient => {
   const listItem = document.createElement('li');
   listItem.classList.add('item');
   listItem.textContent = ingredient;
+return listItem;
 
-  return listItem;
 });
+ console.log(items);
+ ingredientsList.append(...items);
 
-console.log(items);
 
-ingredientsList.append(...items);
- 
- 
+
+
+
