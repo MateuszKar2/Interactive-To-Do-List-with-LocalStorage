0908930425e1f2081905e48dfd7915b0e77a0c6c
MateuszKar2 <system505@protonmail.com>
2023-05-14 20:52:01 +0200

    add task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,24 +1,6 @@
-//napisać skrypt
-// który reaguje na zmianę wartości input#font-size-control" 
-//poprzez zmianę stylu inline span#text, aktualizując właściwość font-size
-//podczas przesuwania suwaka bedzie się zmieniał rozmiar tekstu
-
-
-const input = document.querySelector('#font-size-control');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//tworzę zależność między wielkością liter a przesuwanym suwakiem
+//korzystam z funkcji pomocniczej 
+//ustawiam zdarzenie input, jaki drugi argument przekazująć f.pomocniczą
 
 const changeValue = document.querySelector('#font-size-control');
 const onText = document.querySelector('#text');
@@ -27,9 +9,14 @@ onText.style.fontSize = changeValue.value + 'px';
 
 const textResizing = () => {
     onText.style.fontSize = `${changeValue.value}px`
-};
+}
 
 changeValue.addEventListener('input', textResizing);
+
+
+
+
+
 
 
  
