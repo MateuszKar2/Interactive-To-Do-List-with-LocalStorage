1592005b88466e000f4595c0eef26b0015222110
MateuszKar2 <system505@protonmail.com>
2023-05-14 20:32:10 +0200

    add task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,31 @@
-//napisać skrypt który przy utracie focusu na pole input
-//sprawdza czy wartość wprowadzona przez użytkownika ma odpowiednią długość
-//informacja o liczbie symboli znajduje się w atrybucie data-length
-//jeśli liczba symboli się zgadza border staje się zielony(valid)
-//a jeżeli nie jest czerwony(invalid)
+
 //korzystam z .addEventListener do obsługi zdarzenia
-//
+//zdarzenie blur(utrata focusu na pole input)
+//sprawdzić czy wartość wprowadzona przez użytkownika spełnia warunek walidacji
+//ustawiam zdarzenie blur na zmienną(value) o znanej długości (length) 
+//jeżeli liczba znaków zgadza się z powyższym to dadaje klasę 'valid' (zielony)
+//jeżęli nie, otrzymuje klasę 'invalid' (czerwony)
+
+
+const textInput = document.querySelector('#validation-input');
+
+textInput.addEventListener('blur', event => {
+    if (
+        event.currentTarget.value.length ===
+        Number(event.currentTarget.dataset.length)
+    ) {
+        textInput.classList.add('valid');
+        textInput.classList.remove('invalid');
+    } else {
+        textInput.classList.add('invalid');
+        textInput.classList.remove('valid');
+    }
+})
+
+
+
+
+
 
 
 const textInput = document.querySelector('#validation-input');
