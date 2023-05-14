8f2f28c3e919485c0d99ed6aa846c7178b025586
MateuszKar2 <system505@protonmail.com>
2023-05-14 20:33:48 +0200

    add task 6

--- js/task-06.js
+++ js/task-06.js
@@ -7,26 +7,6 @@
 //jeżęli nie, otrzymuje klasę 'invalid' (czerwony)
 
 
-const textInput = document.querySelector('#validation-input');
-
-textInput.addEventListener('blur', event => {
-    if (
-        event.currentTarget.value.length ===
-        Number(event.currentTarget.dataset.length)
-    ) {
-        textInput.classList.add('valid');
-        textInput.classList.remove('invalid');
-    } else {
-        textInput.classList.add('invalid');
-        textInput.classList.remove('valid');
-    }
-})
-
-
-
-
-
-
 
 const textInput = document.querySelector('#validation-input');
 
