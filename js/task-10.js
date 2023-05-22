5e29f7c4edf45109745d02469003061c1bece88a
MateuszKar2 <system505@protonmail.com>
2023-05-22 13:55:51 +0200

    add fiile now

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,19 @@
+const { create } = require("lodash");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+const boxes = document.addEventListener('#boxes');
+
+function createBoxes(amount) {
+  for (const element of amount ) {
+   const box = document.createElement('div');
+   
+}
+}
+
